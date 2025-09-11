a464848d330de04e54c62610dfd6e0e0e026bc9b
qotrunnad <aartacahjowo@gmail.com>
2025-09-11 15:15:03 +0700

    tabel tailwind

---
+++ src/pages/login.jsx
@@ -0,0 +1,73 @@
+
+
+
+function LoginPage() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        setFormData({ formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('login data:', formData);
+        alert('simulasi login berhasil');
+    };
+
+    return (
+        <div className="flex items-center justify-center min-h-screen ">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+                <h1 className="text-2x1 font-blod text-center mb-6">Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label className="blox text-gray-700 text-sm font-blod mb-2" htmlfor="email">
+                            Email
+                        </label>
+                        <input
+                            className="shadow appereance-none border rounded w-full 
+                        py-2 px-3 text-gray-700 leading-tight focus:outline-one focus:shadow-outline"
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handlechange}
+                            placeholder="Masukan email anda"
+                            required
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-blod mb-2" htmlfor="password">
+                            Password
+                        </label>
+                        <input
+                            className="shadow appreance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline"
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            placeholder="Masukan password anda"
+                            required
+                        />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white
+                        font-blod py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit"
+                        >
+                            Masuk
+                        </button>
+                        <Link to="/register" className="inline-block align-baseline
+                            font-blod text-sm text-blue-500 hover:text-blue-800">
+                            Belum punya akun? Daftar
+                        </Link>
+                    </div>
+                </form>
+            </div >
+        </div >
+    )
+}
---
+++ src/pages/tugas tailwind.jsx
@@ -0,0 +1,146 @@
+import React from 'react'
+import { Tabel } from "Tabeltailwind"
+
+function Tabel () {
+    const data2 =[
+        {id: 1, nama_lengkap: "anindira shireen zulfikar", nama_belakang: "zulfikar", username: "dira"},
+        {id: 2, nama_lengkap: "alfin xavier alexsander", nama_belakang: "alexsander", username: "vier"},
+        {id: 3, nama_lengkap: " muhammad abdurrohman as-shidiqq", nama_belakang: "as-shidiqq", username: "abdur"},
+
+    ]
+    return (
+        <div classname="Tabel">
+        <h5>
+            Tabel 1
+        </h5>
+        <div classname='flex flex-row'>
+        <Tabel striped bordered hover>
+            <thead> 
+                <tr>
+                    <th class="border border-gray-300 ...">#</th>
+                    <th class="border border-gray-300 ...">full name</th>
+                    <th class="border border-gray-300 ...">last name</th>
+                    <th class="border border-gray-300 ...">username</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data2.map((item) => (
+                    <tr>
+                        <td>{item.id}</td>
+                        <td>{item.nama_lengkap}</td>
+                        <td>{item.nama_belakang}</td>
+                        <td>{item.username}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </Tabel>
+
+           <div classname="Tabel">
+            <h5>
+             Tabel 2
+            </h5>
+            <Tabel classname="border-separate border border-gray-400">
+                <thead>
+                    <tr>
+                    <th class="border border-gray-300 ...">#</th>
+                    <th class="border border-gray-300 ...">full name</th>
+                    <th class="border border-gray-300 ...">last name</th>
+                    <th class="border border-gray-300 ...">username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data2.map((item) => (
+                    <tr>
+                        <td>{item.id}</td>
+                        <td>{item.nama_lengkap}</td>
+                        <td>{item.nama_belakang}</td>
+                        <td>{item.username}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </Tabel>
+
+            <div classname="Tabel">
+            <h5>
+             Tabel 3
+            </h5>
+            <Tabel classname="border-separate border border-gray-400">
+                <thead>
+                    <tr>
+                    <th class="border border-gray-300 ...">#</th>
+                    <th class="border border-gray-300 ...">full name</th>
+                    <th class="border border-gray-300 ...">last name</th>
+                    <th class="border border-gray-300 ...">username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data2.map((item) => (
+                    <tr>
+                        <td>{item.id}</td>
+                        <td>{item.nama_lengkap}</td>
+                        <td>{item.nama_belakang}</td>
+                        <td>{item.username}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </Tabel>
+            <div classname="Tabel">
+            <h5>
+            Tabel 4
+            </h5>
+            <Tabel classname="border-separate border border-gray-400">
+                <thead>
+                    <tr>
+                    <th class="border border-gray-300 ...">#</th>
+                    <th class="border border-gray-300 ...">full name</th>
+                    <th class="border border-gray-300 ...">last name</th>
+                    <th class="border border-gray-300 ...">username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data2.map((item) => (
+                    <tr>
+                        <td>{item.id}</td>
+                        <td>{item.nama_lengkap}</td>
+                        <td>{item.nama_belakang}</td>
+                        <td>{item.username}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </Tabel>
+
+            <div classname="Tabel">
+            <h5>
+            Tabel 5
+            </h5>
+            <Tabel classname="border-separate border border-gray-400">
+                <thead>
+                    <tr>
+                    <th class="border border-gray-300 ...">#</th>
+                    <th class="bordeclassr border-gray-300 ...">full name</th>
+                    <th class="border border-gray-300 ...">last name</th>
+                    <th class="border border-gray-300 ...">username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data2.map((item) => (
+                    <tr>
+                        <td>{item.id}</td>
+                        <td>{item.nama_lengkap}</td>
+                        <td>{item.nama_belakang}</td>
+                        <td>{item.username}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </Tabel>
+
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default Tabeltailwind
